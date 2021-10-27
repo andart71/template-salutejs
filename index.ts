96ba0777685e183ba240ada9db2a806b96820ec9
andart71 <one.desiree@yandex.ru>
2021-10-27 10:36:13 +0300

    edit index.ts

--- index.ts
+++ index.ts
@@ -1,21 +1,10 @@
-import express from 'express'
-import cors from 'cors'
-import {handleNlpRequest} from "./src/scenario";
-// require('dotenv').config()
 
-// const PORT = process.env.PORT || 5000
-const PORT = 5000
+import { Router } from 'express'
+import { handleNlpRequest } from './src/scenario'
 
-const app = express()
-app.use(express.json())
-app.use(cors())
+export const apiHookRout = Router()
 
-app.listen(PORT, () => {
-    console.log('server started on port ', PORT)
-    // createIntentsJSON()
-})
-
-app.post('/api/hook', (req, res) => {
-    console.log(req.body);
-    res.status(200).json(handleNlpRequest(req.body))
+apiHookRout.post('/api/hook', async (req, res) => {
+    console.log('api/hook request')
+    res.status(200).json(await handleNlpRequest(req.body))
 })
