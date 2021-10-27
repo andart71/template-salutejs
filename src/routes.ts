025b268c013a5fb47fdeb0377d2a85ae8f1296f3
andart71 <one.desiree@yandex.ru>
2021-10-27 10:45:53 +0300

    edit index.ts

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
-
 import { Router } from 'express'
 import { handleNlpRequest } from './scenario'
 
-export const apiHookRout = Router()
+export const webHookRout = Router()
 
-apiHookRout.post('/api/hook', async (req, res) => {
-    console.log('api/hook request')
+webHookRout.post('/api/hook', async (req, res) => {
+    console.log('api/hook POST request')
     res.status(200).json(await handleNlpRequest(req.body))
+})
+
+webHookRout.get('/api/hook', (req, res) => {
+    console.log('api/hook GET request')
+    res.status(200).json({ message: 'this route need POST method' })
 })
